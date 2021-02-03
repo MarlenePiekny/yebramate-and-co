4e9ad44f50aefd489d04f5dc42dec6c102973b6d
karl-hin <karl.hinterseber@le-campus-numerique.fr>
2021-02-03 12:09:27 +0100

    feat: create view product page

--- resources/views/product/show.php
+++ resources/views/product/show.php
@@ -1,1 +1,19 @@
-<?php
+<main>
+    <div>
+        <h1><?= $productArray['title'] ?></h1>
+    </div>
+    <article>
+        <div>
+            <img src="storage/app/product/mate.jpg">
+        </div>
+        <p><?= $productArray['description'] ?></p>
+        <p><?= $productArray['price'] * $productArray['vat'] ?></p>
+        <form action="/index.php?action=cart" method="post">
+            <div class="mb-3">
+                <label for="number" class="form-label">quantité</label>
+                <input type="number" class="form-control" name="number">
+            </div>
+            <button type="submit" class="btn btn-primary">Ajouter au panier</button>
+        </form>
+    </article>
+</main>
