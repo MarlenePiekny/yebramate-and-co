9559747110e5a41bb5167ec7c5e95a49a67c2ef8
unknown <marlene.piekny@gmail.com>
2021-02-03 15:33:59 +0100

    Feat: home page set up

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,3 +1,14 @@
-<?php
 
-echo "Je suis la page d'accueil";
+<section class="row">
+    <?php foreach ($products as $product): ?>
+        <aside class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $product->title ?></h5>
+                    <a href="/index.php?action=product&id=<?= $product->id?>" class="btn btn-primary">Voir le produit</a>
+                </div>
+            </div>
+        </aside>
+    <?php endforeach; ?>
+</section>
